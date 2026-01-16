fe93ff87542af901be0b139bd3151e61792e878c
Victor Leon <155109085+vileondev@users.noreply.github.com>
2026-01-16 12:05:12 -0300

    Pequenas Alterações

--- Program.cs
+++ Program.cs
@@ -1,47 +1,88 @@
-﻿CarroEletrico meuCarroEletrico = new CarroEletrico();
-meuCarroEletrico.Modelo = "Tesla 3";
-meuCarroEletrico.Abastcer(10);
-meuCarroEletrico.RealizarRevisao();
-Console.WriteLine($"Veículo: {meuCarroEletrico.Modelo} está pronto para uso!");
+﻿// Nome, Preço, Estoque e Peso
+ProdutoFisico fruta = new ProdutoFisico("Melancia", 10.50, 30, 5);
+fruta.ExibirDetalhes();
 
-Caminhao meuCaminhao = new Caminhao();
-meuCaminhao.Modelo = "Caminhão Honda";
-meuCaminhao.Abastcer(100);
-meuCaminhao.RealizarRevisao();
-Console.WriteLine($"Veículo: {meuCaminhao.Modelo} está pronto para uso!");
+public abstract class Produto{
 
-public abstract class Veiculo{
+    // posso ler o nome, mas modifica-lo só com a classe
+    public string Nome {get; private set;}
+    private double _preco;
+    public double Preco{
 
-    protected string Placa{get; set;}
-    public string Modelo{get; set;}
+        get{
+            return _preco;
+        }
 
-    public abstract void Abastcer(double litros);
+        // value é literalmente o valor da variável, também faço uma verificação para alteralo na classe.
+        set
+        {
+            if(value < 0.10)
+            {
+                _preco = 0.10;
+            }
+            else
+            {
+                _preco = value;
+            }
+        }
+    }
+    
+    private int _estoque; 
+    public int Estoque
+    {
+        get
+        {
+            return _estoque;
+        }
+
+        set
+        {
+            if(value < 0)
+            {
+                _estoque = 0;
+            }
+            else
+            {
+                _estoque = value;
+            }
+        }
+    }
+
+    // Construtor da classe, nada muito especial.
+    // Se eu quiser posso adicionar um valor fixo pra uma variável e optar por mudar o valor ou usar um fixo.
+    public Produto(string nome, double precoInicial, int estoqueInicial)
+    {
+        Nome = nome;
+        Preco = precoInicial;
+        Estoque = estoqueInicial;
+    }
+
+    // Virtual simboliza que é uma classe que eu posso "sobrescreve-la" depois
+    public virtual void ExibirDetalhes()
+    {
+        Console.WriteLine($"Nome: {Nome}, Preço: {Preco}, Estoque: {Estoque}");
+    }
+
 }
 
-public interface IManutencao
+// Herança
+// Produto Físico herda os atributos do pai (Produto)
+public class ProdutoFisico : Produto
 {
-    void RealizarRevisao();
-}
+    // Peso é um atributo único da classe filha
+    public double PesoProduto{get; set;}
 
-public class Caminhao : Veiculo, IManutencao{
-    public double CargaMaxima{get; set;}
-
-    public override void Abastecer(double litros){
-        Console.WriteLine($"Abastecendo Caminhão com {litros} litros de Diesel. Custo: R${litros * 6}.")
+    // com base eu tô falando que os atributos que estão sendo referenciados pertecem ao pai
+    public ProdutoFisico(string nome, double preco, int estoque, double peso) : base(nome, preco, estoque)
+    {
+        PesoProduto = peso;
     }
 
-    public void RealizarRevisao(){
-        Console.WriteLine("Revisando freios e suspensão do Caminhão")
+    // Override significa que estou sobrescrevendo uma classe Virtual
+    public override void ExibirDetalhes()
+    {
+        //Base ja ta pegando o que a minha classe virtual BASE tinha e reutilizando
+        base.ExibirDetalhes();
+        Console.WriteLine($"Peso: {PesoProduto}");
     }
-
-}
-
-public class CarroEletrico : Veiculo, IManutencao{
-    public override void Abastecer(double litros){
-        Console.WriteLine($"Carregando bateria com {litros}kWh");
-    }
-
-    public void RealizarRevisao(){
-        Console.WriteLine("Verificando celulas da bateria e software.");
-    }
-}
+ }
